69d0ff4956a051ba070b0929bc242ef7555321cf
toximu <timofei625211@gmail.com>
2026-03-11 22:34:41 +0300

    delete Coffee

--- src/main/java/com/codzilla/backend/repository/CoffeeRepository.java
+++
@@ -1,7 +0,0 @@
-package com.codzilla.backend.repository;
-
-import com.codzilla.backend.controller.Coffee;
-import org.springframework.data.repository.CrudRepository;
-
-public interface CoffeeRepository extends CrudRepository<Coffee , String> {
-}
