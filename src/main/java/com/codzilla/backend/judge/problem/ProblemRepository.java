963d914ce5c3893c8ec932e5567568f4fdacc608
Даниил Калашников <ddkalashnikov@edu.hse.ru>
2026-06-09 11:23:50 +0300

    upd 4

--- src/main/java/com/codzilla/backend/judge/problem/ProblemRepository.java
+++ src/main/java/com/codzilla/backend/judge/problem/ProblemRepository.java
@@ -8,6 +8,6 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface ProblemRepository extends JpaRepository<Problem, Long> {
 
-    @Query(value = "SELECT * FROM problem WHERE problem_type = :problemType AND problem_level = :problemLevel ORDER BY RANDOM() LIMIT 1", nativeQuery = true)
+    @Query(value = "SELECT * FROM problems WHERE problem_type = :problemType AND problem_level = :problemLevel ORDER BY RANDOM() LIMIT 1", nativeQuery = true)
     Problem getRandomProblem(@Param("problemType") String problemType, @Param("problemLevel") String problemLevel);
 }
