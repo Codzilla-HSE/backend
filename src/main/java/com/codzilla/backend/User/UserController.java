f7eb218bf8dd419094076aaf8662f79b5c4fed22
toximu <timofei625211@gmail.com>
2026-06-27 21:44:40 +0300

    fix icon urls

--- src/main/java/com/codzilla/backend/User/UserController.java
+++ src/main/java/com/codzilla/backend/User/UserController.java
@@ -72,7 +72,7 @@ public class UserController {
                 createPresignedGetUrl(
                         s3Settings.bucketName(),
                         "icons/" + user.getEmail()
-                ).replaceFirst("minio", "localhost"),
+                ),
                 user.getId()
         ));
     }
@@ -83,7 +83,7 @@ public class UserController {
         return createPresignedGetUrl(
                 s3Settings.bucketName(),
                 "icons/" + user.getEmail()
-        ).replaceFirst("minio", "localhost");
+        );
     }
 
     @PostMapping("/upload-icon")
@@ -130,7 +130,7 @@ public class UserController {
                 presignedRequest.httpRequest().method()
         );
 
-        return presignedRequest.url().toExternalForm();
+        return presignedRequest.url().toExternalForm().replaceFirst("minio", "localhost");
 
     }
 
