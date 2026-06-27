1b2254697bfcd4c734185fa11a318b9221ebcf45
toximu <timofei625211@gmail.com>
2026-06-27 21:07:54 +0300

    fix dto

--- src/main/java/com/codzilla/backend/judge/problem/ProblemResponseDTO.java
+++ src/main/java/com/codzilla/backend/judge/problem/ProblemResponseDTO.java
@@ -1,12 +1,16 @@
 package com.codzilla.backend.judge.problem;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Data;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.time.LocalDateTime;
 
 @Data
+@NoArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class ProblemResponseDTO {
 
     Long id;
